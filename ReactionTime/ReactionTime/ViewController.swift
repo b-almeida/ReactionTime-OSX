2d5f6aa03c3de28c87661d5690d10df027f35671
Bruno Almeida <Bruno@Brunos-MacBook-Air.local>
2016-04-29 23:12:18 -0500

    Create Xcode project

---
+++ ReactionTime/ReactionTime/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ReactionTime
+//
+//  Created by Bruno on 2016-04-29.
+//  Copyright © 2016 Bruno Almeida. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
